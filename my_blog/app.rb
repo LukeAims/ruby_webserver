74fb876a5ed7284d1a2de4688f584c38aa060818
LukeAims <l.m.aimson@gmail.com>
2023-06-10 05:20:59 +0100

    Add sample text

--- my_blog/app.rb
+++ my_blog/app.rb
@@ -2,12 +2,15 @@ require 'sinatra'
 
 # Sample data for blog posts
 posts = [
-  { title: 'First Post', content: 'This is the content of the first post' },
+  { title: 'Quote of the day!', content: '"Three rings for the Elven kings under the sky, seven for the Dwarf lords in their halls of stone, <br>
+    nine for the mortal men doomed to die, one for the Dark Lord on his dark throne, in the land of Mordor <br>
+    where the shadows lie. One ring to rule them all, one ring to find them, one ring the bring them all, <br>
+    and in the darkness bind them. In the land of Mordor where the shadows lie." -LOTR' },
   { title: 'Second Post', content: 'This is the content of the second post' }
 ]
 
 get '/' do
-  erb :index, locals: { posts: posts }
+  erb :index, locals: { posts:  }
 end
 
 get '/new' do
